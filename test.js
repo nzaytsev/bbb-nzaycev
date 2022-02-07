ff23c33dc52283449c9a33b1fe0325cf781a0d60
nikolai <nikolai@bigbrassband.com>
2022-02-07 16:26:04 +0700

    TST-2 -- test tsx with js

--- test.js
+++ test.js
@@ -3,3 +3,15 @@ import 'prismjs/components/prism-typescript.min'
 import 'prismjs/themes/prism.css'
  
 export {highlightAll, highlightElement}
+
+class AllRepos_ extends React.PureComponent<AllReposProps & AllReposFuns> {
+    render() {
+      const {isAdmin, isNewUI, allReposShown, isLoadingPage, isLoadingRepos, error, onChangeHasUserPat} = this.props
+      return (
+        <Page>
+            {a ? <p>test</p> : null}
+        </Page>
+      )
+    }
+  }
+  
